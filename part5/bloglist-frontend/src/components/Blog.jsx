a37fe0a4632d69a5a36da01b8c131842e3579357
jetkanerva <jeti.kanerva@gmail.com>
2024-03-25 00:03:05 +0200

    Completed 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
+  return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails} style={{ marginLeft: '10px' }}>
+          {detailsVisible ? 'Hide' : 'View'}
+        </button>
+        {detailsVisible && (
+            <div>
+              <p>URL: {blog.url}</p>
+              <p>Likes: {blog.likes} <button>Like</button></p>
+              <p>Added by: {blog.user ? blog.user.name : 'Anonymous'}</p>
+            </div>
+        )}
+      </div>
+  );
+};
+
+export default Blog;
