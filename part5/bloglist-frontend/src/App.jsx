8603a982c01e07f4cd1c7782f2b9c624441d5179
jetkanerva <jeti.kanerva@gmail.com>
2024-03-24 23:05:20 +0200

    Completed parts 5.3 and 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,10 +4,15 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [url, setUrl] = useState(null);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
@@ -31,11 +36,16 @@ const App = () => {
       window.localStorage.setItem(
           'loggedBlogappUser', JSON.stringify(user)
       )
-      setUser(user)
-      setUsername('')
-      setPassword('')
+      setErrorMessage(null);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+      setSuccessMessage('Login successful');
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000);
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('Wrong credentials');
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -48,6 +58,32 @@ const App = () => {
       window.location.reload();
     } catch (exception) {
       console.log(exception);
+    }
+  }
+
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      await blogService.create({
+        title, author, url
+      })
+      blogService.getAll().then(blogs =>
+          setBlogs(blogs)
+      )
+      setTitle('');
+      setAuthor('');
+      setUrl('');
+      setSuccessMessage(`a new blog "${title}" by ${author} added`);
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, 5000);
+    } catch (exception) {
+      console.log(exception);
+      setErrorMessage(exception);
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
   }
 
@@ -76,15 +112,52 @@ const App = () => {
             </div>
             <button type="submit">login</button>
           </form>
+          {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+          {successMessage && <div style={{ color: 'green' }}>{successMessage}</div>}
         </div>
     )
   }
 
   return (
       <div>
+        {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+        {successMessage && <div style={{ color: 'green' }}>{successMessage}</div>}
         <h2>blogs</h2>
         <h4>{user.name} logged in</h4>
         <button onClick={handleLogout}>logout</button>
+        <div>
+          <h2>Create new</h2>
+          <form onSubmit={handleNewBlog}>
+            <div>
+              title
+              <input
+                  type="text"
+                  value={title}
+                  name="title"
+                  onChange={({target}) => setTitle(target.value)}
+              />
+            </div>
+            <div>
+              author
+              <input
+                  type="text"
+                  value={author}
+                  name="author"
+                  onChange={({target}) => setAuthor(target.value)}
+              />
+            </div>
+            <div>
+              url
+              <input
+                  type="text"
+                  value={url}
+                  name="url"
+                  onChange={({target}) => setUrl(target.value)}
+              />
+            </div>
+            <button type="submit">Create</button>
+          </form>
+        </div>
         {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
       </div>
   );
