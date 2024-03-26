d661bc9f2a30c8a64d5ff84ec083c07b1fedffbc
jetkanerva <jeti.kanerva@gmail.com>
2024-03-26 10:55:14 +0200

    Finished part 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders blogs title and author, but not URL or likes by default', () => {
     const blog = {
         title: 'Test Driven Development with React',
         author: 'Jane Doe',
-        url: 'https://example.com/blog/tdd-react',
-        likes: 5,
+        url: 'https://example.com',
+        likes: 4,
         users: [{ name: 'John Doe' }],
-        id: 'blog123'
+        id: '1234'
     }
 
     const mockSetBlogs = vi.fn()
@@ -28,4 +29,30 @@ test('renders blogs title and author, but not URL or likes by default', () => {
 
     const likesElement = screen.queryByText(`Likes: ${blog.likes}`)
     expect(likesElement).toBeNull()
+});
+
+test('displays the URL and number of likes after view button is clicked', async () => {
+    const blog = {
+        title: 'Test Driven Development with React',
+        author: 'Jane Doe',
+        url: 'https://example.com',
+        likes: 4,
+        users: [{ name: 'John Doe' }],
+        id: '1234'
+    };
+
+    const mockSetBlogs = vi.fn();
+    const mockSetErrorMessage = vi.fn();
+    const user = { name: 'John Doe' };
+
+    render(<Blog blog={blog} setBlogs={mockSetBlogs} setErrorMessage={mockSetErrorMessage} user={user} />);
+
+    const viewButton = screen.getByRole('button', { name: 'View' });
+    await userEvent.click(viewButton);
+
+    const urlElement = screen.getByText(`URL: ${blog.url}`);
+    expect(urlElement).toBeDefined();
+
+    const likesElement = screen.getByText(`Likes: ${blog.likes}`);
+    expect(likesElement).toBeDefined();
 });
